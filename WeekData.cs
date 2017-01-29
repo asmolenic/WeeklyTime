427bfa317e8cba1856f3998903ba4ffc61fc3d69
Andrei Smolenic <andrei.smolenic@gmail.com>
2017-01-29 13:03:07 +0200

    Initial commit

---
+++ WeekData.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeklyTimeUtility
+{
+    public class WeekData
+    {
+        private string startDate;               // prefilled - labeling puprose only
+        private string endDate;                 // prefilled - labeling puprose only
+        private bool reported;                  // user managed - should be set in the constructor
+        private string description;             // user managed - should be set in the constructor
+        private TimeSpan hoursWorkedThisWeek;   // user managed - should be set in the constructor
+        private TimeSpan thisWeeksBalance;      // to be computed
+        private TimeSpan balance;               // to be computed
+        private TimeSpan initialBalance;        // user managed - should be set in the constructor
+        private int requiredDays;               // user managed - should be set in the constructor
+        private int hoursPerDay;                // user managed - should be set in the constructor
+
+        //default values
+        private const int defaultRequiredDays = 5;
+        private const int defaultHoursPerDay = 8;
+
+        // default constructor
+        public WeekData()
+        {
+        }
+
+        public WeekData(string _startDate, string _endDate, bool _reported, string _description,
+            TimeSpan _hoursWorkedThisWeek, TimeSpan _initialBalance, 
+            int _requiredDays = defaultRequiredDays, int _hoursPerDay = defaultHoursPerDay)
+        {
+            StartDate = _startDate;
+            EndDate = _endDate;
+            Reported = _reported;
+            Description = _description;
+            HoursWorkedThisWeek = _hoursWorkedThisWeek;
+            InitialBalance = _initialBalance;
+
+            RequiredDays = _requiredDays;
+            HoursPerDay = _hoursPerDay;
+
+            Compute();
+        }
+        
+        // copy constructor
+        public WeekData(WeekData obj)
+        {
+            if (obj != null)
+            {
+                StartDate = obj.StartDate;
+                EndDate = obj.EndDate;
+                Reported = obj.Reported;
+                Description = obj.Description;
+                HoursWorkedThisWeek = obj.HoursWorkedThisWeek;
+                ThisWeeksBalance = obj.ThisWeeksBalance;
+                Balance = obj.Balance;
+                InitialBalance = obj.InitialBalance;
+                RequiredDays = obj.RequiredDays;
+                HoursPerDay = obj.HoursPerDay;
+            }
+        }
+
+        public void Compute()
+        {
+            TimeSpan thisWeeksTarget = new TimeSpan(RequiredDays * HoursPerDay, 0, 0);
+
+            ThisWeeksBalance = HoursWorkedThisWeek.Subtract(thisWeeksTarget);
+            Balance = InitialBalance.Add(ThisWeeksBalance);
+        }
+
+        public override string ToString()
+        {
+            string output = string.Format("[{0}]  {1}-{2}  {3}  {4}  {5}  {6}",
+                reported ? "X" : " ", StartDate, EndDate, (HoursWorkedThisWeek),
+                (ThisWeeksBalance), (Balance), 
+                string.IsNullOrWhiteSpace(Description) ? string.Format("{0} days required", RequiredDays) : Description);
+            string x = TimeSpanToString(HoursWorkedThisWeek);
+            return output + Environment.NewLine;
+        }
+
+        private string TimeSpanToString(TimeSpan input)
+        {
+            string output = string.Empty;
+
+            int totalHours = (int)Math.Floor(input.TotalHours);
+            var remaining = input.Subtract(new TimeSpan(totalHours, 0, 0));
+            int minutes = (int)Math.Floor(remaining.TotalMinutes);
+
+            return string.Format("{0,4}H {1:D2}M", totalHours, minutes);
+        }
+
+        public string StartDate
+        {
+            get
+            {
+                return startDate;
+            }
+
+            set
+            {
+                startDate = value;
+            }
+        }
+
+        public string EndDate
+        {
+            get
+            {
+                return endDate;
+            }
+
+            set
+            {
+                endDate = value;
+            }
+        }
+
+        public bool Reported
+        {
+            get
+            {
+                return reported;
+            }
+
+            set
+            {
+                reported = value;
+            }
+        }
+
+        public TimeSpan ThisWeeksBalance
+        {
+            get
+            {
+                return thisWeeksBalance;
+            }
+
+            set
+            {
+                thisWeeksBalance = value;
+            }
+        }
+
+        public TimeSpan Balance
+        {
+            get
+            {
+                return balance;
+            }
+
+            set
+            {
+                balance = value;
+            }
+        }
+
+        public int RequiredDays
+        {
+            get
+            {
+                return requiredDays;
+            }
+
+            set
+            {
+                requiredDays = value;
+            }
+        }
+
+        public TimeSpan HoursWorkedThisWeek
+        {
+            get
+            {
+                return hoursWorkedThisWeek;
+            }
+
+            set
+            {
+                hoursWorkedThisWeek = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+
+            set
+            {
+                description = value;
+            }
+        }
+
+        public TimeSpan InitialBalance
+        {
+            get
+            {
+                return initialBalance;
+            }
+
+            set
+            {
+                initialBalance = value;
+            }
+        }
+
+        public int HoursPerDay
+        {
+            get
+            {
+                return hoursPerDay;
+            }
+
+            set
+            {
+                hoursPerDay = value;
+            }
+        }
+    }
+}
---
+++ frmMain.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace WeeklyTimeUtility
+{
+    public partial class frmMain : Form
+    {
+        static Random _random = new Random();
+
+        private int hoursPerDay = 8;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("weeks.txt");
+
+            List<string> ranges = new List<string>();
+
+            string line = string.Empty;
+            while ((line = sr.ReadLine()) != null)
+            {
+                ranges.Add(line);
+            }
+
+            sr.Close();
+
+            List<WeekData> weeks = new List<WeekData>();
+
+            TimeSpan balance = TimeSpan.Zero;
+
+
+            string data = string.Empty;
+            foreach (var range in ranges)
+            {
+                string[] tokens = range.Split(new char[] { '-'}, StringSplitOptions.RemoveEmptyEntries);
+                if (tokens.Length < 2)
+                {
+                    continue;
+                }
+
+                var requiredDays = SubstractVacationDays();
+                var workedTime = GenerateWorkedTime(requiredDays);
+
+                WeekData wd = new WeekData(tokens[0].Trim(), tokens[1].Trim(), false, string.Empty,
+                    workedTime, balance, requiredDays);
+                wd.Compute();
+
+                balance = new TimeSpan(wd.Balance.Hours, wd.Balance.Minutes, wd.Balance.Seconds);
+                weeks.Add(wd);
+
+                data += wd.ToString();
+            }
+            lblOutput.Text = data;
+
+            var str = JsonConvert.SerializeObject(weeks);
+            // MessageBox.Show(str);
+        }
+
+        private TimeSpan GenerateWorkedTime(int requiredDays)
+        {
+            var secondsRequired = hoursPerDay * requiredDays * 60 * 60;
+            var variance = secondsRequired / 10;
+            var secondsWorked = _random.Next(secondsRequired - variance, secondsRequired + variance);
+            return new TimeSpan(0, 0, secondsWorked);
+        }
+
+        private int SubstractVacationDays()
+        {
+            int p = 5; // 5% chances to introduce vacation days
+
+            List<bool> roulette = new List<bool>();
+
+            for (int i = 0; i < 100 - p; i++)
+            {
+                roulette.Add(false);
+            }
+
+            for (int i = 0; i < p; i++)
+            {
+                roulette.Add(true);
+            }
+
+            Shuffle<bool>(roulette);
+
+            bool shouldAddVacationDays = roulette[_random.Next(0, roulette.Count)];
+
+            if (shouldAddVacationDays)
+            {
+                return _random.Next(0, 5);
+            }
+
+            return 5;
+        }
+
+        static void Shuffle<T>(List<T> deck)
+        {
+            int n = deck.Count;
+            for (int i = 0; i < n; i++)
+            {
+                // NextDouble returns a random number between 0 and 1.
+                // ... It is equivalent to Math.random() in Java.
+                int r = i + (int)(_random.NextDouble() * (n - i));
+                T t = deck[r];
+                deck[r] = deck[i];
+                deck[i] = t;
+            }
+        }
+
+    }
+}
